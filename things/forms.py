0a28ed1ffecdb22ce4b2fd060061199ddf3e1a55
Ubayd Khan <ubaydkh@gmail.com>
2022-12-09 15:15:43 +0000

    Complete assignment

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,24 @@
 """Forms of the project."""
 
+from django import forms
+
+from things.models import Thing
+
 # Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'max_length': '35'
+            }),
+            'description': forms.Textarea(attrs={
+                'max_length': '120'
+            }),
+            'quantity': forms.NumberInput(attrs={
+                'type': 'number',
+                'min': '0',
+                'max': '50'
+            })
+        }
--- things/views.py
+++ things/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.contrib import messages
+
+from things.forms import ThingForm
 
 def home(request):
+    form = ThingForm()
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Submission was successful.')
+        else:
+            messages.error(request, 'Invalid submission!')
+            messages.error(request, form.errors)
     return render(request, 'home.html', {'form': form})
